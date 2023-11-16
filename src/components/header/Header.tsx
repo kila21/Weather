f691927663eaec375164ea87f878b8c3dab50818
kila <luka.kilasonia.1@btu.edu.ge>
2023-11-16 15:37:32 +0400

    header component init

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="countries">
+      <span>Tbilisi</span>
+      <span>Rustavi</span>
+      <span>London</span>
+      <span>Tokyo</span>
+      <span>Paris</span>
+    </div>
+  );
+};
