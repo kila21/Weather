9a8e2a185096e626b9e8483ce918d19de1084b3d
kila <luka.kilasonia.1@btu.edu.ge>
2023-11-16 17:45:17 +0400

    display component and create date object

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { useEffect } from "react";
+// import axios from "axios";
+
+import { Search } from "./components/search/search";
+import { Header } from "./components/header/Header";
+
 import "./App.css";
 
 function App() {
+  // const apikey = import.meta.env.VITE_API_KEY;
+
+  // const api = `https://api.openweathermap.org/geo/1.0/direct?q=rustavi&appid=${apikey}`;
+  const newDate = new Date();
+
+  useEffect(() => {
+    // axios.get(api).then((dat) => {
+    //   console.log(dat);
+    // });
+    // console.log(apikey);
+  }, []);
   return (
-    <>
-      <div>Weather App</div>
-    </>
+    // layout
+    <div className="main-container">
+      <Header />
+      <Search />
+      <div className="date">
+        {newDate.toLocaleString("en-US", { weekday: "long" })},
+        {newDate.getDate()} &nbsp;
+        {newDate.toLocaleString("en-US", { month: "long" })} &nbsp;
+        {newDate.getFullYear()} &nbsp;| &nbsp; Local time: {newDate.getHours()}:
+        {newDate.getMinutes()}
+      </div>
+    </div>
   );
 }
 
